4c9520da1111c934aeda31f74162756aa579e82c
Russell J. Dow <russell.j.dow@avanade.com>
2018-09-28 10:22:02 -0700

    added some filters to test quote searching

--- src/app/search-results-page/search-results-page.component.ts
+++ src/app/search-results-page/search-results-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../../data/models/domain/quote';
+import { QuoteService } from '../quote.service';
+import { filter } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search-results-page',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-results-page.component.scss']
 })
 export class SearchResultsPageComponent implements OnInit {
+  quotes: Quote[];
+  quotes$: Observable<Quote[]>;
+  filters: any;
 
-  constructor() { }
+  constructor(private quoteService: QuoteService) { }
 
   ngOnInit() {
+    this.quotes$ = this.quoteService.getQuotes();
   }
 
+  onQuoteIdInput(quoteId: number){
+    this.quotes$.pipe(
+      filter((quote: Quote) => quote.id == quoteId)
+    ).subscribe((quotes: Quote[]) => this.quotes = quotes);
+  }
+
+  onPriceMinInput(min: number){
+    this.quotes$.pipe(
+      filter((quote: Quote) => quote.price >= min)
+    ).subscribe((quotes: Quote[]) => this.quotes = quotes);
+  }
+
+  onPriceMaxInput(max: number){
+    this.quotes$.pipe(
+      filter((quote: Quote) => quote.price >= max)
+    ).subscribe((quotes: Quote[]) => this.quotes = quotes);
+  }
 }
